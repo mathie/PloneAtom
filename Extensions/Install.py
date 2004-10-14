c3e5d484d466a8a4af0811222747145eba023313
Graeme Mathieson <mathie@woss.name>
2004-10-14 20:15:36 +0000

    Add an action and icon in the install script.
    git-svn-id: file:///Users/mathie/Development/old-svn/projects/PloneAtom/trunk@907 d5f202f0-b0d6-0310-8bd8-c586a511d7e9

--- Extensions/Install.py
+++ Extensions/Install.py
@@ -1,6 +1,8 @@
 from Products.Archetypes.public import listTypes
 from Products.Archetypes.Extensions.utils import installTypes, \
      install_subskin
+from Products.CMFCore.utils import getToolByName
+from Products.CMFCore import CMFCorePermissions
 from cStringIO import StringIO
 
 from Products.PloneAtom.config import *
@@ -9,5 +11,32 @@ def install(self):
     out = StringIO()
     installTypes(self, out, listTypes(PKG_NAME), PKG_NAME)
     install_subskin(self, out, GLOBALS)
+
+    setupActions(self)
+    
     print >> out, "Successfully installed %s." % PKG_NAME
     return out.getvalue()
+
+def setupActions(obj):
+    """ Add the atom action and actionicon. """
+    at = getToolByName(obj, 'portal_actions')
+    ait = getToolByName(obj, 'portal_actionicons')
+    for action in at.listActions():
+        if action.getId() == 'atom':
+            break
+    else:
+        at.addAction(id = 'atom',
+                     name = "Atom feed of this folder's contents",
+                     action = "string:$object_url/ATOM",
+                     condition = "python:portal.portal_syndication.isSyndicationAllowed(object)",
+                     permission = CMFCorePermissions.View,
+                     category = 'document_actions',
+                     visible = True)
+    for ai in ait.listActionIcons():
+        if ai.getActionId() == 'atom':
+            break
+    else:
+        ait.addActionIcon(category = 'plone',
+                          action_id = 'atom',
+                          icon_expr = 'atom.gif',
+                          title = 'Atom Syndication')
